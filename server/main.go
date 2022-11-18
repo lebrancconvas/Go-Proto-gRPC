d3ad6b98ba81904b127334887859ef809593428a
lebrancconvas <lebrancconvas@gmail.com>
2022-11-19 05:15:34 +0700

    ✨ feat: implementation

--- server/main.go
+++ server/main.go
@@ -1,5 +1,17 @@
 package main
 
+import (
+	"github.com/lebrancconvas/Go-Proto-gRPC/pb/proto"  
+	"fmt" 
+)
+
 func main() {
-	
+	a := proto.Mage{
+		Name: "Lebranc",
+		Level: 1,
+		Element: "Fire",
+		Health: 100,
+		Mana: 50,
+	}	
+	fmt.Println(a.Name);  
 }
